30dca113b66ab1105d968af36d5930f349c50d54
RockeZzZas <60551075+RockeZzZas@users.noreply.github.com>
2020-07-07 00:25:11 +0300

    generatedURLS

--- routes/httpClient.js
+++ routes/httpClient.js
@@ -5,18 +5,29 @@ process.env.FLICKR_API_KEY = "00ac5f70d662304b87e7da585bbdef9d"
 let flickr = new Flickr(process.env.FLICKR_API_KEY);
 
 const myfunc = () => {
+  var address = [];
   flickr.photos.search({
     text: 'doggo'
   }).then((res) => {
     res.body.photos.photo.forEach((photo) => {
-      console.log(photo.server)
-    })
+      
+      let frmID = photo.farm;
+      let srvID = photo.server;
+      let phID = photo.id;
+      let scrt = photo.secret;
+      let photoAddress = 'https://farm'+frmID+'.staticflickr.com/'+srvID+'/'+phID+'_'+scrt+'.jpg';
+      address.push(photoAddress);
+      console.log(address);
+      
+      return address;
+    })    
   }).catch((err) => {
     console.error('bonk', err);
   });
+  
 };
 
-
+ 
 module.exports = {
   myfunc: myfunc
 };
